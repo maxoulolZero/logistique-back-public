9d07cdbee32f0cad64d0135f344179b3db65b627
samy.nalbandian <samy.nalbandian@epita.fr>
2021-12-31 14:14:31 +0100

    update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { StocksModule } from './api/stocks/stocks.module';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        PGHOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
         PORT: Joi.number(),
       }),
     }),
